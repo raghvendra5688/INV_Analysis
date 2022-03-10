b6fcc98320dbe116a32dd9589c91f8a4c5f3355d
rmall <administrator@rmall.stjude.sjcrh.local>
2022-03-10 13:10:29 -0600

    Revised the consensus clustering part and have new set of cancers with invasiveness detrimental phenotype

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 In the scripts folder, just run each R script after installing all the required R packages to obtain each result figure.
 
-Each result figure will appear in Results/Paper_Figures/Figure_i.pdf , where i represents figure number.
+Each result figure will appear in Results/Revised_Figures/Revised_Figure_(v2)_i.pdf , where i represents figure number.
 
-Text results are provided in Results/Paper_Text_Results/ folder. Similarly, figures are provided in Results/Paper_Figures/ folder.
+Text results are provided in Results/Revised_Text_Results/ folder. Similarly, figures are provided in Results/Revised_Figures/ folder.
 
 The data used for all the experiments is also available at: Mall, Raghvendra (2020), “Transcriptomic Dataset for Network based identification of key Master Regulators for Immunologic Constant of Rejection”, Mendeley Data, V3, doi: 10.17632/d9ffb7kkzt.3
 
-#Data should be downloaded and all its content should be added to the 'Data' folder within main INV_Analysis repository for running R scripts.
+#Data should be downloaded and all its content should be added to the 'Data' folder within main ICR_Analysis repository for running R scripts.
 
 In the Data/Others/ folder, please gunzip the me_net_full.Rdata.gz file.
 
@@ -14,4 +14,4 @@ In the Data/PRECOG/ folder, please put the es_list1.rds and es_list2.rds files f
 
 Run the misc_figures.R in scripts/ folder for validation results on the PRECOG repository.
 
-Run the INV_Consensus_Classification.R followed by High_Medium_Low_INV_classification.R in scripts/ folder to get the INV-High, INV-Medium and INV-Low labels for each cancer sample per cancer type.
+Run the ICR_Consensus_Classification.R followed by High_Medium_Low_ICR_classification.R in scripts/ folder to get the ICR-High, ICR-Medium and ICR-Low labels for each cancer sample per cancer type.
--- scripts/INV_Consensus_Classification.R
+++ scripts/INV_Consensus_Classification.R
@@ -23,8 +23,8 @@ source("scripts/ipak.function.R")
 source("scripts/get_functions.R")
 source("scripts/High_Medium_Low_INV_classification.R")
 
-required.packages = c("RCurl","httr", "rjson", "stringr", "HGNChelper")
-required.bioconductor.packages = c("clue","ConsensusClusterPlus")
+required.packages = c("RCurl","httr", "rjson", "stringr", "HGNChelper","ggplot2")
+required.bioconductor.packages = c("clue","ConsensusClusterPlus","survivalAnalysis","dplyr")
 ipak(required.packages)
 ibiopak(required.bioconductor.packages)
 
@@ -106,4 +106,242 @@ for (Cancer in Cancers)
   save(table_cluster_assignment,optimal.calinsky, file = outputname)
   
 }
+
+#Perform the survival analysis w.r.t. PFI based on the invasiveness categories
+###############################################################################
+pb <- txtProgressBar(title = "Processing Cancers for Survival", min = 0, max = length(Cancers), width=300)
+survival_list <- list()
+for (i in 1:length(Cancers))
+{
+  Cancer <- Cancers[i] 
+  
+  #Get the curated survival information 
+  clinical_df <- read.table(paste0("Data/survival_data/survival_",Cancer,"_survival.txt"),header=T,sep="\t")
+  clinical_df <- clinical_df[!is.na(clinical_df$OS.time),]
+  if (sum(is.na(clinical_df$Redaction)>0))
+  {
+    clinical_df <- clinical_df[is.na(clinical_df$Redaction),]  
+  }else{
+    clinical_df <- clinical_df[clinical_df$Redaction!="Redacted",]
+  }
+  
+  #Load the table information with the clusters
+  load(paste0("Data/",Cancer,"/",Cancer,"_INV_cluster_assignment_k2-6_v2.Rdata"))
+  
+  #Get the survival information w.r.t. the INV Score
+  clinical_df$INVscore <- rep(NA, nrow(clinical_df))
+  clinical_df$INV_Pheno <- rep(NA, nrow(clinical_df))
+  clinical_df$TCGA_Match_Id <- rep(NA, nrow(clinical_df))
+  rownames_table <- rownames(table_cluster_assignment)
+  for (j in 1:nrow(clinical_df))
+  {
+    sample_id <- clinical_df[j,]$sample
+    table_sample_id <- which(grepl(sample_id, rownames_table))
+    if (length(table_sample_id)>0)
+    {
+      clinical_df[j,]$INVscore <- table_cluster_assignment[table_sample_id,]$INVscore
+      clinical_df[j,]$INV_Pheno <- table_cluster_assignment[table_sample_id,]$HML_cluster
+      clinical_df[j,]$TCGA_Match_Id <- rownames_table[table_sample_id]
+    }
+  }
+  clinical_df <- clinical_df[!is.na(clinical_df$INVscore),]
+  clinical_df  <- clinical_df[!is.na(clinical_df$TCGA_Match_Id),]
+  clinical_df <- clinical_df[clinical_df$OS.time<=3650,]
+  clinical_df <- clinical_df[clinical_df$INV_Pheno!="INV Medium",]
+  
+  ##Find the optimal cutpoint based on the INV Score
+  #res.cut <- surv_cutpoint(data=clinical_df, time="PFI.time", event="PFI", variables = "INVscore", minprop=0.1, progressbar = T)
+  #res.cat <- surv_categorize(res.cut, variables = "INVscore", labels=c("INV Low","INV High"))
+  #clinical_df$INV_Pheno <- res.cat$INVscore
+  
+  #Make the survival plot based on High vs Low categories
+  print(Cancer)
+  fit <- analyze_survival(data=clinical_df,time_status = vars(OS.time, OS), by = INV_Pheno)
+  print(fit)
+  survival_list[[i]] <- fit
+  g <- kaplan_meier_plot(fit, 
+                         break.time.by="breakByYear",
+                         xscale = 365.25,
+                         risk.table = F,
+                         pval = F,
+                         hazard.ratio=F,
+                         xlab = "Time (in Years)",
+                         palette=c("red","blue"),
+                         ggtheme=ggplot2::theme_bw(base_size=10))
+  g$plot <- g$plot+theme(panel.grid.major = element_blank(),
+                         panel.grid.minor = element_blank(),
+                         text = element_text(size=10, colour="black" ),
+                         axis.text.x = element_text(size=10,  colour = "black"),
+                         axis.text.y = element_text(size=10,  colour = "black" ),
+                         axis.title.x = element_text(size=10,  colour = "black"),
+                         axis.title.y = element_text(size=10,  colour = "black"),
+                         plot.title = element_text(size = 10,  colour="black"),
+                         axis.text = element_text(size=10,  colour="black"))
+  ggsave(filename=paste0("Results/",Cancer,"/",Cancer,"_Survival_Categorization.pdf"), plot = print(g$plot, newpage = FALSE), device = cairo_pdf(pointsize = 10), height=2, width=3.5, units="in", dpi=300)
+  dev.off()
+  write.table(clinical_df, file=paste0("Results/",Cancer,"/",Cancer,"_survival_info.csv"),row.names=F, col.names=T, sep="\t", quote=F)
+  
+  setTxtProgressBar(pb, i, label=paste0("Done for Cancer: ",Cancer))
+  
+}
+names(survival_list) <- Cancers
+
+#Make the forest plot data frame
+########################################################################
+library(dplyr)
+library(tcltk)
+library(forestplot)
+
+mean <- NULL
+UI <- NULL
+LI <- NULL
+cancer_info <- NULL
+pval_info <- NULL
+hazards_info <- NULL
+invhigh_info <- NULL
+invlow_info <- NULL
+for (i in 1:length(survival_list)){
+  cancer <- names(survival_list)[i]
+  fit_info <- survival_data_frames(survival_list[[i]])
+  temp_mean <- as.numeric(fit_info$hazardRatios[2,"HR"])
+  temp_ui <- as.numeric(fit_info$hazardRatios[2,"Upper.CI"])
+  temp_li <- as.numeric(fit_info$hazardRatios[2,"Lower.CI"])
+  if (!is.infinite(temp_ui) & !is.infinite(temp_li))
+  {
+    temp_pval <- as.character(fit_info$hazardRatios[2,"p"])
+    temp_invhigh <- as.character(fit_info$strataMetadata$records[1])
+    temp_invlow <- as.character(fit_info$strataMetadata$records[2])
+    if (as.numeric(temp_invhigh)+as.numeric(temp_invlow)>=0)
+    {
+      if (temp_pval=="<0.001")
+      {
+        temp_pval <- paste0(temp_pval," ***")
+      }
+      else{
+        temp_pval <- as.numeric(temp_pval)
+        if (temp_pval<=0.1 & temp_pval>0.05)
+        {
+          temp_pval <- paste0(round(temp_pval,2)," *")
+        }
+        else if (temp_pval<=0.05 & temp_pval>=0.001)
+        {
+          temp_pval <- paste0(temp_pval," **")
+        }
+        else if (temp_pval>0.1)
+        {
+          temp_pval <- paste0(temp_pval)
+        }
+      }
+      mean <- c(mean, temp_mean)
+      UI <- c(UI, temp_ui)
+      LI <- c(LI, temp_li)
+      pval_info <- c(pval_info, temp_pval)
+      cancer_info <- c(cancer_info, cancer)
+      invhigh_info <- c(invhigh_info, temp_invhigh)
+      invlow_info <- c(invlow_info, temp_invlow)
+      hazards_info <- c(hazards_info, paste0(temp_mean,"(",temp_li,"-",temp_ui,")"))
+    }
+  }
+}
+base_data <- tibble(Cancer = cancer_info,
+                    N.INV.High = invhigh_info,
+                    N.INV.Low = invlow_info,
+                    P.value = pval_info,
+                    HR = hazards_info,
+                    mean = mean,
+                    upper = UI,
+                    lower = LI,
+)
+base_data <- base_data[order(mean, decreasing=F),]
+header <- tibble(Cancer = "Cancer",
+                 N.INV.High = "N1",
+                 N.INV.Low = "N2",
+                 P.value = "P-value",
+                 HR = "HR(95%CI)"
+)
+output_df <- bind_rows(header,base_data)
+
+#Make the plot for all cancers
+fn_uni_full <- local({
+  i = 0
+  b_clrs = c(rep("blue",3),rep("red",29))
+  
+  function(..., clr.line, clr.marker){
+    i <<- i + 1
+    fpDrawNormalCI(..., clr.line = b_clrs[i], clr.marker = b_clrs[i])
+  }
+})
+
+output_df %>% 
+  forestplot(labeltext = c(Cancer, N.INV.High, N.INV.Low, P.value, HR), 
+             fn.ci_norm = fn_uni_full,
+             graph.pos = 6,
+             boxsize = 0.25,
+             xlog = TRUE,
+             graphwidth = unit(2,"cm"),
+             colgap = unit(2,"mm"),
+             #col = fpColors(box = "blue",line = "blue"),
+             lwd.xaxis=1,
+             hrzl_lines = list("2" = gpar(lty = 2), 
+                               "5" = gpar(lwd = 1, columns = 1:6, col = "#000044")),
+             lineheight = unit(5,"mm"),
+             txt_gp = fpTxtGp(ticks=gpar(fontfamily="sans",fontsize=20),
+                              label=gpar(fontfamily="sans", fontsize=10),
+                              title=gpar(fontfamily="sans", fontsize=10, fontface="bold"))) -> p_full
+pdf("Results/PanCancer/All_Cancers_of_Interest_Invasivenss_Forest_Plot_Supp.pdf", width = 6, height=8, pointsize = 11)
+p_full
+dev.off()
+
+#Make the Boxplot highlight the PANoptosis score for all cancers
+################################################################################
+boxplot_df <- NULL
+diff_mean_low_high <- NULL
+for (Cancer in Cancers)
+{
+  load(paste0("Data/",Cancer,"/",Cancer,"_INV_cluster_assignment_k2-6_v2.Rdata"))
+  mean_low <- mean(table_cluster_assignment[table_cluster_assignment$HML_cluster=="INV Low",]$INVscore)
+  mean_high <- mean(table_cluster_assignment[table_cluster_assignment$HML_cluster=="INV High",]$INVscore)
+  diff_mean_low_high <- c(diff_mean_low_high,mean_high)
+  temp <- cbind(rep(Cancer,nrow(table_cluster_assignment)),table_cluster_assignment$INVscore,table_cluster_assignment$HML_cluster)
+  boxplot_df <- rbind(boxplot_df, temp)
+}
+boxplot_df <- as.data.frame(boxplot_df)
+colnames(boxplot_df) <- c("Cancer","INVscore","INV_Category")
+boxplot_df$Cancer <- as.character(as.vector(boxplot_df$Cancer))
+boxplot_df$INVscore <- as.numeric(as.vector(boxplot_df$INVscore))
+boxplot_df$INV_Category <- as.character(as.vector(boxplot_df$INV_Category))
+boxplot_df$INV_Category <- factor(boxplot_df$INV_Category, levels=c("INV Low","INV Medium","INV High"))
+names(diff_mean_low_high) <- Cancers
+diff_mean_low_high <- sort(diff_mean_low_high, decreasing = F)
+boxplot_df$Cancer <- factor(boxplot_df$Cancer, levels = names(diff_mean_low_high))
+
+#subset_boxplot_df <- boxplot_df[boxplot_df$Cancer %in% c("LGG","KIRC","SKCM","STAD","COAD"),]
+#subset_boxplot_df$Cancer <- factor(subset_boxplot_df$Cancer, levels=c("LGG","KIRC","SKCM","STAD","COAD"))
+
+#g_boxplot <- ggplot(data=subset_boxplot_df, aes(x=Cancer, y=PANoptosis_Score, fill=PANoptosis_Category))+
+#  geom_boxplot(position=position_dodge(1)) + scale_fill_manual(name="", values=c("blue","yellow","red"))+
+#  theme_bw() + xlab("Cancers") + ylab("PANoptosis Score")+ylim(c(-1,1))+
+#  theme(plot.background=element_rect(fill = "white"),
+#        panel.background = element_rect(fill = 'white'),legend.background = element_rect(fill = "white", color = NA),
+#        legend.key = element_rect(color = "gray", fill = "white"), legend.title = element_text(color = "black"), legend.text = element_text(size=11, family="Arial", color = "black"),
+#        axis.text.x = element_text(size=11, angle=90, hjust = 0.5, vjust=0.5, family="Arial"),
+#        axis.text.y = element_text(size=11, family="Arial"),
+#        axis.title.x = element_text(size=11, family="Arial"),
+#        axis.title.y = element_text(size=11, family="Arial"))
+#ggsave(filename = "Results/PanCancer/Sub_Cancers_of_Interest_Boxplot_PANoptosis_Score.pdf", plot = g_boxplot, device = pdf(fonts="Arial"), width = 5, height=3, units="in", dpi=450)
+#dev.off()
+
+g_boxplot <- ggplot(data=boxplot_df, aes(x=Cancer, y=INVscore, fill=INV_Category))+
+  geom_boxplot(position=position_dodge(1)) + scale_fill_manual(name="", values=c("blue","yellow","red"))+
+  theme_bw() + xlab("Cancers") + ylab("Invasiveness Score")+ylim(c(4,16))+
+  theme(plot.background=element_rect(fill = "white"),
+        panel.background = element_rect(fill = 'white'),legend.background = element_rect(fill = "white", color = NA),
+        legend.key = element_rect(color = "gray", fill = "white"), legend.title = element_text(color = "black"), legend.text = element_text(size=10, family="sans", color = "black"),
+        axis.text.x = element_text(size=10, angle=90, hjust = 0.5, vjust=0.5, family="sans", color="black"),
+        axis.text.y = element_text(size=10, family="sans",color="black"),
+        axis.title.x = element_text(size=10, family="sans", color="black"),
+        axis.title.y = element_text(size=10, family="sans", color="black"))
+ggsave(filename = "Results/PanCancer/All_Cancers_of_Interest_Boxplot_Invasiveness_Score.pdf", plot = g_boxplot, device = pdf(fonts="sans"), width = 9, height=4, units="in", dpi=300)
+dev.off()
+
 
